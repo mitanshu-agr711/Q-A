e4576cb400d5ff9a6fdb0a9d7a30b495b0253321
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2025-02-01 12:03:40 +0530

    schema is defined

---
+++ src/model/q/a.schema.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface context extends Document {
+  question: string;
+  answer: string;
+  translations: {
+    [key: string]: {
+      question: string;
+      answer: string;
+    };
+  };
+}
+
+const quesans = new Schema({
+  question: { type: String, required: true },
+  answer: { type: String, required: true },
+  translations: {
+    type: Map,
+    of: {
+      question: { type: String },
+      answer: { type: String }
+    }
+  }
+});
+
+export default mongoose.model<context>('FAQ', quesans);
